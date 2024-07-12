75c11390c89bfced4c23f1eeeb6b3c766eecf4f2
adrian92612 <adrian.villamin0612@gmail.com>
2024-07-12 20:18:23 +0800

    added category model

---
+++ models/category.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Category", CategorySchema);
---
+++ populatedb.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+// get arguments passed on command line
+
+const userArgs = process.argv.slice(2);
