a9730d696316b18b5535f4542c627d8d9bb5950b
adrian92612 <adrian.villamin0612@gmail.com>
2024-07-14 14:17:42 +0800

    creating a category now works

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -49,6 +49,26 @@ export const categoryDetails = asyncHandler(async (req, res, next) => {
 });
 
 export const categoryCreate_get = (req, res, next) =>
-  res.render(`category_form`, { title: `Create a Category` });
+  res.render(`category_form`, { title: `Add a Category` });
 
-export const categoryCreate_post = [];
+export const categoryCreate_post = [
+  // validate and sanitize
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified").trim().isLength({ min: 1 }).escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render(`category_form`, {
+        title: `Add a Category`,
+        category: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const newCategory = await Category.create(req.body);
+      res.redirect(newCategory.url);
+    }
+  }),
+];
